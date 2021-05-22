cd264cb966aebee2228aa4874c0e8abaedde63e6
Thiago Tenório <thiago.tenorio@rogalabs.com>
2021-05-22 18:26:01 -0300

    feat(auth): add authentication callback

--- app/controllers/api/v2/base_controller.rb
+++ app/controllers/api/v2/base_controller.rb
@@ -2,4 +2,14 @@
 
 class Api::V2::BaseController < ApplicationController
   include ExceptionHandler
+
+  before_action :authenticate
+
+  def authenticate
+    url = ENV['API_URL'] + '/auth/get_token'
+
+    response = HTTParty.get(url)
+  
+    @token = response['token']
+  end
 end
